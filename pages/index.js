5e695e21bec2db9e868746951b85f18b2f98a616
Tauhidul Islam Robin <robin13690@gmail.com>
2023-01-11 23:34:37 +0600

    client part

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Client from "../Components/Client/Client";
 
 
 export default function Home() {
   return (
     <>
       <h1>Hello World</h1>
+      {/* <Client></Client> */}
     </>
   )
 }
