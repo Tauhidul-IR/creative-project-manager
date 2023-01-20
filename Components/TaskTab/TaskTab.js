5cd3fc6aecdc738d060ff2dfc45efd16c4c46a51
jakiasultana2022 <rokeyasbd@gmail.com>
2023-01-20 11:52:12 +0600

    button style

--- Components/TaskTab/TaskTab.js
+++ Components/TaskTab/TaskTab.js
@@ -7,29 +7,32 @@ const TaskTab = () => {
         <div>
                  <div className='className=" mt-16'>
             <Tab.Group manual>
-                <Tab.List className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1  rounded-xl  p-1">
-                    <Tab className={({ selected }) => classNames("w-full rounded-lg py-5 text-2xl font-medium leading-5 text-blue-700", selected ? "underline text-black " : "text-blue-700")}>Create Task</Tab>
+                <Tab.List className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1  ">
+                 
 
-                    <Tab className={({ selected }) => classNames("w-full rounded-lg py-5 text-2xl font-medium leading-5 text-blue-700", selected ? "underline text-black " : "text-blue-700")}>Complete Task</Tab>
+                    <Tab className={({ selected }) => classNames("  py-5 text-2xl font-medium leading-5 text-blue-700", selected ? "underline text-black " : "text-blue-700")}> Create  Task</Tab>
 
-                    <Tab className={({ selected }) => classNames("w-full rounded-lg py-5 text-2xl font-medium leading-5 text-blue-700", selected ? "underline text-black " : "text-blue-700")}>Pending Task</Tab>
+                    <Tab className={({ selected }) => classNames("  py-5 text-2xl font-medium leading-5 text-blue-700", selected ? "underline text-black " : "text-blue-700")}>Complete Task</Tab>
+
+                    <Tab className={({ selected }) => classNames(" py-5 text-2xl font-medium  text-blue-700", selected ? "underline text-black " : "text-blue-400")}>Pending Task</Tab>
                 </Tab.List>
 
-                <Tab.Panels className="mt-5  ">
-                    <Tab.Panel className="rounded-xl  p-3">
-                   <div >
+                <Tab.Panels>
+
+                    <Tab.Panel >
+                   <div className='grid place-items-center' >
                <TaskForm></TaskForm>
                    </div>
                     </Tab.Panel>
 
 
-                    <Tab.Panel className="rounded-xl bg-white p-3">
+                    <Tab.Panel >
                     <div className='grid place-items-center'>
                     <h2 className=' text-black'> Complete Task Coming</h2>
                     </div>
                     </Tab.Panel>
 
-                    <Tab.Panel className="rounded-xl bg-white p-3">
+                    <Tab.Panel >
                     <div className='grid place-items-center'>
                     <h2 className=' text-black'> Pending Task Coming</h2>
                     </div>
