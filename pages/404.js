96d06df8270cbee2b81fc706a1b5b1121fb87d04
Tauhidul Islam Robin <robin13690@gmail.com>
2023-01-24 11:39:16 +0600

    update navbar

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import img from '../public/images/404-error.jpg'
+
+
+const Error = () => {
+    return (
+        <div>
+            {/* <img src={img} className="w-full h-screen" alt="" /> */}
+        </div>
+    );
+};
+
+export default Error;
