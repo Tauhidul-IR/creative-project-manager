ed706ea71b607f6631b25bcf05b2870e315b8204
Tauhidul Islam Robin <robin13690@gmail.com>
2023-01-27 11:24:36 +0600

    delete a folder

--- pages/Features/features.js
+++ pages/Features/features.js
@@ -1,14 +1,14 @@
-import AddGantt from "../../Components/Gantt/AddGantt";
+// import AddGantt from "../../Components/Gantt/AddGantt";
 
 
 
-const features = () => {
-    return (
-        <div>
-            <h2 className="text-center text-3xl mb-28">Features is coming</h2>
-            <AddGantt></AddGantt>
-        </div>
-    );
-};
+// const features = () => {
+//     return (
+//         <div>
+//             <h2 className="text-center text-3xl mb-28">Features is coming</h2>
+//             <AddGantt></AddGantt>
+//         </div>
+//     );
+// };
 
-export default features;
+// export default features;
