1c9966bf3e4597cb5e2445c9a3f47048a9e4a473
Tauhidul Islam Robin <robin13690@gmail.com>
2023-01-27 11:05:40 +0600

    goal icon change1

--- pages/Features/features.js
+++ pages/Features/features.js
@@ -5,8 +5,8 @@ import AddGantt from "../../Components/Gantt/AddGantt";
 const features = () => {
     return (
         <div>
-           <h2 className="text-center text-3xl mb-28">Features is coming</h2> 
-       <AddGantt></AddGantt>
+            <h2 className="text-center text-3xl mb-28">Features is coming</h2>
+            {/* <AddGantt></AddGantt> */}
         </div>
     );
 };
