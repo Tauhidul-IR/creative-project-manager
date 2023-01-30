f160b50e2b80e5fcea88ee4e1eba509e117a5615
Tauhidul Islam Robin <robin13690@gmail.com>
2023-01-30 11:23:21 +0600

    delete users

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,5 +1,5 @@
-import { users } from "../../../users";
+// import { users } from "../../../users";
 
-export default function handler (req, res){
-    res.status(200).json(users)
-}
+// export default function handler(req, res) {
+//     res.status(200).json(users)
+// }
