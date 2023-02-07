9aeb6b9fdbd1cf7a0239c1e005e38159dad3b5fe
Tauhidul Islam Robin <robin13690@gmail.com>
2023-02-07 22:50:36 +0600

    dashboard update

--- Components/Dashboard/DashboardPageCover.js
+++ Components/Dashboard/DashboardPageCover.js
@@ -4,10 +4,10 @@ const DashboardPageCover = () => {
     return (
         <div>
             <div>
-                <div className="hero min-h-[85px] ">
+                {/* <div className="hero min-h-[85px] ">
                     <div className="hero-overlay bg-cyan-900"></div>
 
-                </div>
+                </div> */}
             </div>
         </div>
     );
