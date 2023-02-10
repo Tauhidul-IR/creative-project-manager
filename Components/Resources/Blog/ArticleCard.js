be63bcdd6ac56b104a1e5c2371288bd61cecf701
jakiasultana2022 <rokeyasbd@gmail.com>
2023-02-10 17:14:43 +0600

    blog2

--- Components/Resources/Blog/ArticleCard.js
+++ Components/Resources/Blog/ArticleCard.js
@@ -1,26 +1,23 @@
 const ArticleCard = ({article}) => {
     return (
         <div>
-           	<div class="containerCard">
+           	<div class="containerCard ">
 			<div class="cardArticle">
 				<div class="content">
 					<div class="imgBx">
 						<img src={article.img}/>
 					</div>
 					<div class="contentBx">
-						<h3>{article.title}</h3>
-                        <p><span className="text-sky-500 ml-10">By </span>{article.writer}</p>
-                        <p className="ml-10 mt-3">{article.date}</p>
-                      
+						<h3>{article.title}<br/><span>By {article.writer}</span></h3>
 					</div>
 				</div>
 				<ul class="sci">
-					<li>
-                    <p className="ml-10 mt-3 mb-3">{article.description.slice(0,90)}</p>
+					<li >
+						<a href="">{article.description.slice(0,50)}</a>
 					</li>
 					
 				</ul>
-			</div> 
+			</div>
         </div>
         </div>
     );
