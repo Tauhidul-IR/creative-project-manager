d153a125362bab8aca9fb54d5cc6377092638988
shishir <eftekharAhmedshishir@gmail.com>
2023-02-25 21:52:58 +0600

    25/02/23 my work

--- Components/Dashboard/TopCards.jsx
+++ Components/Dashboard/TopCards.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
 function TopCards() {
+    const[projects,setProjects] = useState([]);
+
+    useEffect(() => {
+        fetch('https://creative-project-manager-server.vercel.app/create-project')
+        .then(res=>res.json())
+        .then(data => setProjects(data));
+    },[]);
+    
     return (
         <div className='grid lg:grid-cols-5 gap-4 p-4'>
             <div className='lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg'>
@@ -15,12 +23,12 @@ function TopCards() {
             </div>
             <div className='lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg' >
             <div className='flex flex-col w-full pb-4'>
-                    <p className='text-2xl font-bold'>$177,390</p>
-                    <p className='text-gray-600'>YTD Revenue</p>
+                    <p className='text-2xl font-bold'>{projects.length}</p>
+                    <p className='text-gray-600'>Total Projects</p>
 
                 </div>
                 <p className='bg-green-200 flex justify-center items-center p-2 rounded-lg'>
-                    <span className='text-green-700 text-lg font-bold'>+30</span>
+                    <span className='text-green-700 text-lg font-bold'>10+</span>
                 </p>
             </div>
             <div className=' bg-white flex justify-between w-full border p-4 rounded-lg'>
--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div>
             <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-                <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
+                <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-1 lg:grid-cols-4">
                     <div className="sm:col-span-2">
                         <h2 className='font-bold text-black text-4xl'>
                             CP<span className='text-sky-300'>M</span>
